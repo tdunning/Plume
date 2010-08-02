12fbd9c657a5a0dddf284d81fabb02dfedb20956
Ted Dunning <ted.dunning@gmail.com>
2010-08-02 08:17:43 -0700

    Added apache copyright notice

--- src/main/java/com/tdunning/plume/local/eager/LocalPlume.java
+++ src/main/java/com/tdunning/plume/local/eager/LocalPlume.java
@@ -1,10 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tdunning.plume.local.eager;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import com.tdunning.plume.*;
-import com.tdunning.plume.local.eager.LocalCollection;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,7 +41,7 @@ public class LocalPlume extends Plume {
 
   @Override
   public <T> PCollection<T> readAvroFile(String name, Class<T> target) {
-    return null;  //To change body of implemented methods use File | Settings | File Templates.
+    return null;  // To change body of implemented methods use File | Settings | File Templates.
   }
 
   @Override
